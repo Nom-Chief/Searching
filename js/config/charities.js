af28e2e580b27ef6b1fed6460f13e52c20312d25
Chaim <gwhaaat@gmail.com>
2025-01-27 12:10:31 +0200

    Remove deprecated charity assets and update charity configuration to Stepping Stones and Yeshiva Neve Tzion

--- js/config/charities.js
+++ js/config/charities.js
@@ -1,69 +1,30 @@
 const CHARITIES = {
-    meirpanim: {
-        name: 'Meir Panim',
-        value: 'meirpanim',
-        url: 'https://www.msearch.co/pse/search',
-        logo: 'Charity/Meir_Panim/logo/logo.png',
-        background: 'Charity/Meir_Panim/background/background.png',
-        inputName: 'query',
+
+    SteppingStones: {
+        name: 'Stepping Stones',
+        value: 'stepping-stones',
+        url: 'https://src.searchzes.com/click',
+        logo: 'Charity/Stepping_Stones/logo/logo.png',
+        background: 'Charity/Stepping_Stones/background/background.png',
+        inputName: 'p10',
         hiddenInputs: {
-            spid: '150',
-            sspid: '1013',
-            channel: '3'
-        }
-    },
-    levHatorah: {
-        name: 'Lev Hatorah',
-        value: 'lev-hatorah',
-        url: 'https://www.msearch.co/pse/search',
-        logo: 'Charity/Lev_Hatorah/logo/logo.png',
-        background: 'Charity/Lev_Hatorah/background/background.png',
-        inputName: 'query',
-        hiddenInputs: {
-            spid: '150',
-            sspid: '1013',
-            channel: '2'
-        }
-    },
-    chabad: {
-        name: 'Chabad',
-        value: 'chabad',
-        url: 'https://www.msearch.co/pse/search',
-        logo: 'Charity/Chabad/logo/logo.png',
-        background: 'Charity/Chabad/background/background.png',
-        inputName: 'query',
-        hiddenInputs: {
-            spid: '150',
-            sspid: '1013',
-            channel: '1'
+            campaign_id: '382423',
+            pub_id: '34'
         }
     },
     yeshivaNeveTzion: {
         name: 'Yeshiva Neve Tzion',
         value: 'yeshiva-neve-tzion',
-        url: 'https://www.msearch.co/pse/search',
+        url: 'https://src.searchzes.com/click',
         logo: 'Charity/Yeshiva_Neve_Tzion/logo/logo.png',
         background: 'Charity/Yeshiva_Neve_Tzion/background/background.png',
-        inputName: 'query',
+        inputName: 'p10',
         hiddenInputs: {
-            spid: '150',
-            sspid: '1013',
-            channel: '5'
+            campaign_id: '382423',
+            pub_id: '34'
         }
     },
-    NachumSegalNetwork: {
-        name: 'Nachum Segal Network',
-        value: 'nachum-segal-network',
-        url: 'https://www.msearch.co/pse/search',
-        logo: 'Charity/Nachum_Segal_Network/logo/logo.png',
-        background: 'Charity/Nachum_Segal_Network/background/background.png',
-        inputName: 'query',
-        hiddenInputs: {
-            spid: '150',
-            sspid: '1013',
-            channel: '4'
-        }
-    }
+
 };
 
 export default CHARITIES; 
